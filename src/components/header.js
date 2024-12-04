6237f4b429f1650fca07b1fc142872ea34ff327b
Pericles Feitoza <periclesanfe@gmail.com>
2024-12-03 21:16:17 -0300

    feat: enhance header with new navigation buttons for Grupos, Salas, and Reservas; update styles for button components

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,16 @@ import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 import logoJogaJunto from "../assets/logoJogaJunto.png";
 import imagemPerfil from "../assets/Perfil.png";
+import gruposImage from "../assets/gruposLogo.png"; // Imagem para "Grupos"
+import salasImage from "../assets/salasLogo.png"; // Imagem para "Salas"
+import reservasImage from "../assets/reservasLogo.png"; // Imagem para "Reservas"
 import "../styles/components/Header.css"; // Importa o arquivo CSS
 
 const Header = ({ onLoginClick, buttonRef }) => {
   return (
     <Navbar bg="light" expand="lg" className="shadow-sm">
       <Container>
-        {/* Logo */}
+        {/* Logo à esquerda */}
         <Navbar.Brand as={Link} to="/">
           <img
             src={logoJogaJunto}
@@ -20,13 +23,45 @@ const Header = ({ onLoginClick, buttonRef }) => {
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        
 
-        {/* Links e Botões no Header */}
+        {/* Centralizando os botões compostos (Grupos, Salas, Reservas) */}
+        <Nav className="mx-auto d-flex justify-content-center">
+          <Nav.Item className="mx-2">
+            <Button className="btn-composto">
+              <img
+                src={gruposImage} // Imagem para "Grupos"
+                alt="Grupos"
+                className="btn-composto-img"
+              />
+              <span className="btn-composto-text">Grupos</span>
+            </Button>
+          </Nav.Item>
+
+          <Nav.Item className="mx-2">
+            <Button className="btn-composto">
+              <img
+                src={salasImage} // Imagem para "Salas"
+                alt="Salas"
+                className="btn-composto-img"
+              />
+              <span className="btn-composto-text">Salas</span>
+            </Button>
+          </Nav.Item>
+
+          <Nav.Item className="mx-2">
+            <Button className="btn-composto">
+              <img
+                src={reservasImage} // Imagem para "Reservas"
+                alt="Reservas"
+                className="btn-composto-img"
+              />
+              <span className="btn-composto-text">Reservas</span>
+            </Button>
+          </Nav.Item>
+        </Nav>
+
+        {/* Links e Botões à direita */}
         <Nav className="ml-auto">
-          {/* Link para o Gestor de Quadra */}
-          <Nav.Link as={Link} to="/gestor-quadra">Anuncie sua arena</Nav.Link>
-
           {/* Botão "Entrar" com cor verde */}
           <Nav.Item>
             <Button
