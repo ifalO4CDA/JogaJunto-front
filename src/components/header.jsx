896701443e9dcbe92c71031e1a12fb76ae46a261
ANDRÉ FILIPE GOMES ROCHA <116861543+Andrefgr24@users.noreply.github.com>
2024-12-17 23:23:54 -0300

    feat/telaReservas

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -47,7 +47,7 @@ const Header = ({ onLoginClick, buttonRef }) => {
             </Button>
           </Navbar.Brand>
 
-          <Navbar.Brand as={Link} to="/reservas" className="mx-2">
+          <Navbar.Brand as={Link} to="/arena/reserva" className="mx-2">
             <Button className="btn-composto">
               <img
                 src={reservasImage} // Imagem para "Reservas"
